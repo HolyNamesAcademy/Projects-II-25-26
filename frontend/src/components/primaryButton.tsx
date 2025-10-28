0ac99099927c29d51a04010b82183e8d22c88201
Alexander Bell-Towne <alexander@bell-towne.com>
2025-10-28 07:45:43 -0700

    Add primary button component

---
+++ frontend/src/components/primaryButton.tsx
@@ -0,0 +1,10 @@
+export default function PrimaryButton({ text }: { text: string }) {
+  return (
+    <a
+      className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-blue-600 text-white gap-2 hover:bg-blue-700 font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+      href="/api-demo"
+    >
+      {text}
+    </a>
+  )
+}
