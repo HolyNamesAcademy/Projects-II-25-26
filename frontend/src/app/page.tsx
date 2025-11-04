38278b9746ecbc54a21b866a434bc1509a6a2fdd
Alexander Bell-Towne <alexander@bell-towne.com>
2025-11-04 07:19:33 -0800

    Fix primary button

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,6 +5,9 @@ export default function Home() {
   return (
     <div>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <h1 className="text-5xl">
+          Welcome
+        </h1>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
       </footer>
