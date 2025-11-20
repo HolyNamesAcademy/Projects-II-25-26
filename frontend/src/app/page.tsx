e3e37fbc0b95b1cfdd70db2518355442ec051b66
ClaireHermitage <chermitage26@holynames-sea.org>
2025-11-20 08:46:37 -0800

    Formatted button and text for opening page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,10 +4,11 @@ import PrimaryButton from "@/components/primaryButton";
 export default function Home() {
   return (
     <div>
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-5xl">
-          Welcome
-        </h1>
+      <main className="flex flex-col gap-[32px] row-start-2 items-center h-screen align-middle justify-center">
+          <h1 className="text-5xl align-middle">
+            Welcome to HNA Swapeeee!
+          </h1>
+          <PrimaryButton text="Shop All Items" href ="/shop" />
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
       </footer>
