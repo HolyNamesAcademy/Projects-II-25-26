0eae94efb4e29c0221cdbc2d75afcfde8eeec49d
juliajurui <juliazha@andrew.cmu.edu>
2025-11-20 09:35:17 -0800

    vix view item page image rendering

--- frontend/src/app/viewitem/page.tsx
+++ frontend/src/app/viewitem/page.tsx
@@ -65,79 +65,50 @@ function ItemInfo ({contact, size, cost}: {contact: string, size: string, cost: 
             <p
               className="text-2xl font-extrabold text-gray-900 sm:text-3xl dark:text-white"
             >
-              ${cost}
-            </p>
-
-          
-            </div>
-          </div>
-
-          <div className="mt-6 sm:gap-4 sm:items-center sm:flex sm:mt-8">
-            <a
-              href="#"
-              title=""
-              className="flex items-center justify-center py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-              role="button"
-            >
-              <svg
-                className="w-5 h-5 -ms-2 me-2"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M12.01 6.001C6.5 1 1 8 5.782 13.001L12.011 20l6.23-7C23 8 17.5 1 12.01 6.002Z"
-                />
+        flfgrribo0kay-1diuringalin5svgborcrysMcodark:bhiddenntactthhunderbolt256GB0ee0assName17keg00ded2msdiv>
+     flfgrribo0kay-1diuringalin5svgborcrysMcodark:bhiddenntactthhunderbolt256GB0ee0assName17keg00ded2ms-center sm:flfgrribo0kay-1diuringalin5svgborcrysMcodark:bhiddenntactthhunderbolt256GB0ee0assName17keg00ded2      title=""
+fgrribo0kay-1diuringalin5svgborcrysMcodark:bhiddenntactthhunderbolt256GB0ee0assName17keg00ded2.5 px-5 text-sm fgrribo0kay-1diuringalin5svgborcrysMcodark:bhiddenntactthhunderbolt256GB0ee0assName17keg00ded-lg border border-grribo0kay-100 ringalin5svgborcrysMcodark:bhiddenntactthhunderbolt256GB0ee0assName17keg00-10 focus:ring-4 focus:ribo0k:focus:ringalin5svgborcrysMcodark:bhiddenntactthhunderbolt256GB0ee0assName17keg00 dark:text-gray-400 dark:bo0 dark:hover:alin5svgborcrysMco dark:hiddenntactthhunderbolt256GB0ee0assName17keg-gray-700"
+              role          >
+ alin5svgborcrysMco dark:hiddenntactthhunderbolt256GB0ee0assName17ke           className="w-5 h-5 -              alin5svgborcrysMco dark:hiddenntactthhunderbolt256GB0ee0assName17ke           xmlns="http://www.w3
+              lin5svgborcrysMco dark:hiddenntactthhunderbolt256GB0ee0assName17ke      heigh="24"
+                              lin5svgborcrysMco dark:hiddenntactthhunderbolt256GB0ee0assName17ke          >                <path
+               lin5svgborcrysMco dark:hiddenntactthhunderbolt256GB0ee0assName17ke   stroke-lecap="round"
+                  stroke-lin5svgborcrysMco dark:hiddenntactthhunderbolt256GB0ee0assName17ke-width="2"
+                d="M12.01 6.001C6.5 1 1 8 5svgborcrysMco dark:hiddenntactthhunderbolt256GB0ee0assName17.5 1 12.01 6.002Z"
+              />
               </svg>
-              Add to favorites
-            </a>
+svgborcrysMco dark:hiddenntactthhunderbolt256GB0ee0assName         </a>
 
             <a
-              href="#"
-              title=""
-              className="text-white mt-4 sm:mt-0 bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 flex items-center justify-center"
-              role="button"
-            >
+            href="#"
+           svgborcrysMco dark:hiddenntactthhunderbolt256GB0ee0assName="text-white mt-4 sm:mt-0 bg-primary-700 her:bg-primary-800 focus:svgborcrysMco dark:hiddenntactthhunderbolt256GB0ee0 font-medium rounded-lg text-sm px-5 py-2.5 dark:-primary-600 dark:hover:svgborcrysMco dark:hiddenntactthhunderbolt256GB0ee-none dark:focus:ring-primary-800 flex items-centejustify-center"
+        svgborcrysMco dark:hiddenntactthhunderbolt256GB0e     >
               <svg
-                className="w-5 h-5 -ms-2 me-2"
-                aria-hidden="true"
+                classNamew-5 h-5 -ms-2 me-2"
+     svgborcrysMco dark:hiddenntactthhunderbolt256GB0e"
                 xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="none"
+              width="24"
+ svgborcrysMco dark:hiddenntactthhunderbolt256GB0                fill="none"
                 viewBox="0 0 24 24"
-              >
-                <path
-                  stroke="currentColor"
+            >
+     svgborcrysMco dark:hiddenntactthhunderbolt256GB0       stroke="currentColor"
                   stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M4 4h1.5L8 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm.75-3H7.5M11 7H6.312M17 4v6m-3-3h6"
+                svgborcrysMco dark:hiddenntactthhunderbolt256GB0             stroke-width="2"
+                  d="M4 4h1.5L8 16m0 0h8m-8 0a2 2 0 1 0 0 svgborcrysMco dark:hiddenntactthhunderbolt256GB0 4 2 2 0 0 0 0-4Zm.75-3H7.5M11 7H6.312M17 4v6m-3-3h6"
                 />
-              </svg>
-
-              Add to cart
+              </svgborcrysMco dark:hiddenntactthhunderbolt256GB
             </a>
           </div>
 
-          <hr className="my-6 md:my-8 border-gray-200 dark:border-gray-800" />
-
-          <p className="mb-6 text-gray-500 dark:text-gray-400">
-            Studio quality three mic array for crystal clear calls and voice
+          <hr className="my-6 md:my-8 border-gray-200 dark:borcrysMco dark:hiddenntactthhunderbolt256GB <p className="mb-6 text-gray-500 dark:text-gray-400">
+            Studio quality three mic array for crysMco dark:hiddenntactthhunderbolt256GB
             recordings. Six-speaker sound system for a remarkably robust and
-            high-quality audio experience. Up to 256GB of ultrafast SSD storage.
+            high-quality audiMco dark:hiddenntactthhunderbolt256GB of ultrafast SSD storage.
           </p>
 
           <p className="text-gray-500 dark:text-gray-400">
-            Two Thunderbolt USB 4 ports and up to two USB 3 ports. Ultrafast
-            Wi-Fi 6 and Bluetooth 5.0 wireless. Color matched Magic Mouse with
+       Mco dark:hiddenntactthhunderbolt USB 4 ports and up to two USB 3 ports. Ultrafast
+            Wi-Fi 6 and Bluetooth 5.0 wireless. Color matched Magic Mco dark:hiddenntactth
             Magic Keyboard or Magic Keyboard with Touch ID.
         </p>
       </div>
@@ -148,12 +119,12 @@ function ItemInfo ({contact, size, cost}: {contact: string, size: string, cost: 
 
 function ItemInfo({ contact, size, cost }: {contact: string, size: string, cost: string}) {
   return (
-  <section className="min-h-screen flex flex-col justify-between mx-4 py-8 bg-white md:py-16 dark:bg-gray-900 antialiased">
+  <section className="min-h-screen flex flex-col justify-between mx dark:hidden-4 py-8 bg-white md:py-16 dark:bg-gray-900 antialiased">
       <div className="max-w-screen-xl px-4 mx-auto 2xl:px-0">
-        <div className="lg:grid lg:grid-cols-2 lg:gap-8 xl:gap-16">
+        <div className="lg:grid dark:hidden lg:grid-cols-2 lg:gap-8 xl:gap-16">
           <div className="shrink-0 max-w-md lg:max-w-lg mx-auto">
 
-            <img
+            <Image
               className="w-full dark:hidden rounded-lg object-cover"
               src="/images/whiteknitsweater-demo.png"
               alt="white knit sweater picture"
@@ -243,10 +214,14 @@ function ItemInfo({ contact, size, cost }: {contact: string, size: string, cost:
         </div>
       </div>
 
-      <PrimaryButton 
-              text="🪙 Buy" 
-              href="/viewitem" 
-            />
+      <div className="w-full flex justify-center pb-6">
+        <div className="max-w-xs w-full">
+          <PrimaryButton
+            text="🪙 Buy"
+            href="/viewitem"
+          />
+        </div>
+      </div>
 
     </section>
   );
