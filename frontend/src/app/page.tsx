6c3a3f41558e36047430c35855fda7e43b38800b
ClaireHermitage <chermitage26@holynames-sea.com>
2025-12-04 07:17:02 -0800

    added items buttons to the home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -8,7 +8,31 @@ export default function Home() {
           <h1 className="text-5xl align-middle">
             Welcome to HNA Swapeeee!
           </h1>
-          <PrimaryButton text="Shop All Items" href ="/shop" />
+          <div className="grid grid-cols-2 gap-8">
+            <div className="flex items-center justify-center">
+              <a href="#" aria-label="Tops">
+                <img className="rounded-full w-36 h-36 object-cover" src="/images/Tops.png" alt="Tops" />
+              </a>
+            </div>
+            <div className="flex items-center justify-center">
+              <a href="#" aria-label="Dresses">
+                <img className="rounded-full w-36 h-36 object-cover" src="/images/Dresses.png" alt="Dresses" />
+              </a>
+            </div>
+            <div className="flex items-center justify-center">
+              <a href="#" aria-label="Bottoms">
+                <img className="rounded-full w-36 h-36 object-cover" src="/images/Bottoms.png" alt="Bottoms" />
+              </a>
+            </div>
+            <div className="flex items-center justify-center">
+              <a href="#" aria-label="Shoes">
+                <img className="rounded-full w-36 h-36 object-cover" src="/images/Shoes.png" alt="Shoes" />
+              </a>
+            </div>
+            <div className="flex items-center justify-center">
+              <PrimaryButton text="Shop All Items" href="#" />
+            </div>
+          </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
       </footer>
