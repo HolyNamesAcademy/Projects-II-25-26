86a8128d77935957f508f2400ab6f708292a41d8
juliajurui <juliazha@andrew.cmu.edu>
2026-02-19 07:44:29 -0800

    ran ./sail frontend:lint and :format to clean up code

--- frontend/src/components/categoryFilter.tsx
+++ frontend/src/components/categoryFilter.tsx
@@ -3,19 +3,14 @@
 import { useState } from "react";
 import PrimaryButton from "@/components/primaryButton";
 
-export default function CategoryFilter(){
-   return (
-    <>
-        <div className="flex items-center justify-center py-4 md:py-8 flex-wrap gap-3">
-            <PrimaryButton text="All categories" type="button" />
-            <PrimaryButton text="Tops" type="button" />
-            <PrimaryButton text="Bottoms" type="button" />
-            <PrimaryButton text="Dresses" type="button" />
-            <PrimaryButton text="Shoes" type="button" />
-        </div>
-    </>
-   ) 
-    
-
+export default function CategoryFilter() {
+  return (
+    <div className="flex items-center justify-center py-4 md:py-8 flex-wrap gap-3">
+      <PrimaryButton text="All categories" type="button" />
+      <PrimaryButton text="Tops" type="button" />
+      <PrimaryButton text="Bottoms" type="button" />
+      <PrimaryButton text="Dresses" type="button" />
+      <PrimaryButton text="Shoes" type="button" />
+    </div>
+  );
 }
-
