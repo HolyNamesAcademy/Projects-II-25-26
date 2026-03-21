c804603d0f61d09a697fb27df813b9fa27cf2904
Alexander Bell-Towne <alexander@bell-towne.com>
2026-03-21 11:44:45 -0700

    Remove Cart

--- frontend/src/components/navMenu.tsx
+++ frontend/src/components/navMenu.tsx
@@ -159,33 +159,6 @@ export default function NavMenu() {
               </Link>
             </li>
 
-            <li>
-              <Link
-                href="/cart"
-                className="flex items-center px-2 py-1.5 text-body rounded-base hover:bg-neutral-tertiary hover:text-fg-brand group"
-              >
-                <svg
-                  className="w-6 h-6 text-gray-800 dark:text-white"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7H7.312"
-                  />
-                </svg>
-
-                <span className="flex-1 ms-3 whitespace-nowrap">Cart</span>
-              </Link>
-            </li>
-
             {!authLoading && !isLoggedIn && (
               <>
                 <li>
